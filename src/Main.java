e5fd6dd7be0f836f9219de97573cabfd3a1c1b7d
Bojana Rankovic <bojana.nu@gmail.com>
2015-04-07 22:05:56 +0200

    Igrica sada pocinje sa ekranom iste boje kao i meni

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import states.Menu;
 public class Main {
     public static void main(String[] args) {
         GameHost host = new GameHost("Hexy", 1100, 700);
+        host.setBackgroundClearColor(Menu.BACKGROUND_COLOR);
         new Menu(host);
         host.setState("menu");
     }
